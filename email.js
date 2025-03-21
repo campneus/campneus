2689f95a1b103d77e8e212b956306076d1f39b88
campneus <edgard.freitas@pirelli.com>
2025-03-21 08:45:23 -0300

    Update email.js

--- email.js
+++ email.js
@@ -2,7 +2,7 @@
     var script = document.createElement("script");
     script.src = "https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js";
     script.onload = function() {
-        console.log("EmailJS carregado com sucesso!");
+        console.log("Disparo carregado com sucesso!");
         if (typeof emailjs !== "undefined") {
             emailjs.init("bnv9mOuJF0AUUuTV4"); // USER ID
         }
